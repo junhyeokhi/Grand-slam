9746faa9828e169da2f32b50fa0aa3ccf6d74236
ka6865 <ka6865@naver.com>
2026-04-08 18:02:33 +0900

    회원가입 UI초안

---
+++ app/form.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
+
+class UserCreateForm(FlaskForm):
+    email = EmailField('이메일', validators=[
+        DataRequired('이메일은 필수 항목입니다.'),
+        Email()
+    ])
+    nickname = StringField('닉네임', validators=[
+        DataRequired('닉네임은 필수 항목입니다.'),
+        Length(min=2, max=50)
+    ])
+    password1 = PasswordField('비밀번호', validators=[
+        DataRequired('비밀번호는 필수 항목입니다.'),
+        EqualTo('password2', message='비밀번호가 일치하지 않습니다.')
+    ])
+    password2 = PasswordField('비밀번호 확인', validators=[
+        DataRequired('비밀번호 확인은 필수 항목입니다.')
+    ])
+    phone = StringField('전화번호', validators=[
+        DataRequired('전화번호는 필수 항목입니다.')
+    ])
+    # 주소는 카카오 API로 입력받으므로 StringField로
+    address = StringField('주소', validators=[
+        DataRequired('주소는 필수 항목입니다.')
+    ])
+
+
+class UserLoginForm(FlaskForm):
+    email = StringField('이메일', validators=[DataRequired('이메일은 필수 항목입니다.'), Email()])
+    password = PasswordField('비밀번호', validators=[DataRequired('비밀번호는 필수 항목입니다.')])
--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 
+from app.form import UserCreateForm
+
 # 블루프린트 생성.
 # url_prefix='/auth를 설정하면 모든 주소 앞에 /auth가 자동
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -14,7 +16,8 @@ def login():
 # 3. 회원가입 페이지 연결 (주소: /auth/signup)
 @bp.route('/signup/')
 def signup():
-    return render_template('auth/signup.html')
+    form = UserCreateForm()
+    return render_template('auth/signup.html', form=form)
 
 
 # 4. 마이페이지 연결 (주소: /auth/mypage)
